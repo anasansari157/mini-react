5c1ab65efec147ba7b829f3ee2e773d835bcdd61
Anas Ansari <anasansari157@gmail.com>
2020-04-01 11:03:53 +0530

    Make createElement

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object" ? child : createTextElement(child)
+      )
+    }
+  };
+}
