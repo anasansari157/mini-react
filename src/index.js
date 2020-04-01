49aff5b8deb1eedf5574d4a0f41c9cbffe2a7bdb
Anas Ansari <anasansari157@gmail.com>
2020-04-01 11:10:22 +0530

    Complete React

--- src/index.js
+++ src/index.js
@@ -170,3 +170,111 @@ function updateFunctionComponent(fiber) {
 	const children = [fiber.type(fiber.props)];
 	reconcileChildren(fiber, children);
 }
+
+function useState(initial) {
+	const oldHook =
+		wipFiber.alternate &&
+		wipFiber.alternate.hooks &&
+		wipFiber.alternate.hooks[hookIndex];
+	const hook = {
+		state: oldHook ? oldHook.state : initial,
+		queue: []
+	};
+
+	const actions = oldHook ? oldHook.queue : [];
+	actions.forEach((action) => {
+		hook.state = action(hook.state);
+	});
+
+	const setState = (action) => {
+		hook.queue.push(action);
+		wipRoot = {
+			dom: currentRoot.dom,
+			props: currentRoot.props,
+			alternate: currentRoot
+		};
+		nextUnitOfWork = wipRoot;
+		deletions = [];
+	};
+
+	wipFiber.hooks.push(hook);
+	hookIndex++;
+	return [hook.state, setState];
+}
+
+function updateHostComponent(fiber) {
+	if (!fiber.dom) {
+		fiber.dom = createDom(fiber);
+	}
+	reconcileChildren(fiber, fiber.props.children);
+}
+
+function reconcileChildren(wipFiber, elements) {
+	let index = 0;
+	let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+	let prevSibling = null;
+
+	while (index < elements.length || oldFiber != null) {
+		const element = elements[index];
+		let newFiber = null;
+
+		const sameType = oldFiber && element && element.type == oldFiber.type;
+
+		if (sameType) {
+			newFiber = {
+				type: oldFiber.type,
+				props: element.props,
+				dom: oldFiber.dom,
+				parent: wipFiber,
+				alternate: oldFiber,
+				effectTag: "UPDATE"
+			};
+		}
+		if (element && !sameType) {
+			newFiber = {
+				type: element.type,
+				props: element.props,
+				dom: null,
+				parent: wipFiber,
+				alternate: null,
+				effectTag: "PLACEMENT"
+			};
+		}
+		if (oldFiber && !sameType) {
+			oldFiber.effectTag = "DELETION";
+			deletions.push(oldFiber);
+		}
+
+		if (oldFiber) {
+			oldFiber = oldFiber.sibling;
+		}
+
+		if (index === 0) {
+			wipFiber.child = newFiber;
+		} else if (element) {
+			prevSibling.sibling = newFiber;
+		}
+
+		prevSibling = newFiber;
+		index++;
+	}
+}
+
+const Didact = {
+	createElement,
+	render,
+	useState
+};
+
+/** @jsx Didact.createElement */
+function Counter() {
+	const [state, setState] = Didact.useState(1);
+	return (
+		<h1 onClick={() => setState((c) => c + 1)} style="user-select: none">
+			Count: {state}
+		</h1>
+	);
+}
+const element = <Counter />;
+const container = document.getElementById("root");
+Didact.render(element, container);
