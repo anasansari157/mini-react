15b5e3a34ad5fed3a2c7e9b0a400222ce1aaa539
Anas Ansari <anasansari157@gmail.com>
2020-04-01 11:21:06 +0530

    Refactor Commits

--- src/index.js
+++ src/index.js
@@ -36,7 +36,8 @@ const isProperty = (key) => key !== "children" && !isEvent(key);
 const isNew = (prev, next) => (key) => prev[key] !== next[key];
 const isGone = (prev, next) => (key) => !(key in next);
 function updateDom(dom, prevProps, nextProps) {
-	//Remove old or changed event listeners
+
+
 	Object.keys(prevProps)
 		.filter(isEvent)
 		.filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
@@ -45,22 +46,23 @@ function updateDom(dom, prevProps, nextProps) {
 			dom.removeEventListener(eventType, prevProps[name]);
 		});
 
-	// Remove old properties
+
+	
 	Object.keys(prevProps)
 		.filter(isProperty)
 		.filter(isGone(prevProps, nextProps))
 		.forEach((name) => {
 			dom[name] = "";
 		});
-	// Set new or changed properties
+
+	
 	Object.keys(nextProps)
 		.filter(isProperty)
 		.filter(isNew(prevProps, nextProps))
 		.forEach((name) => {
 			dom[name] = nextProps[name];
 		});
-
-	// Add event listeners
+	
 	Object.keys(nextProps)
 		.filter(isEvent)
 		.filter(isNew(prevProps, nextProps))
